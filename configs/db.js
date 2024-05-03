6d102e109dd654407fa9c7543e0a10f2ea3fc630
Payal Singh <112754449+itspayalsingh@users.noreply.github.com>
2024-05-03 13:48:00 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,10 +1,12 @@
-const {Sequelize}= require("sequelize")
+// const {Sequelize}= require("sequelize")
+// const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { Sequelize } = require("sequelize-cockroachdb");
 require("dotenv").config()
- 
-const seqlize=new Sequelize( process.env.MYSQL_DATABASE,process.env.MYSQL_USER,process.env.MYSQL_ROOT_PASSWORD,{
-    host:process.env.MYSQL_HOST,
-    dialect:"mysql"
-})
-// console.log("seqllll",seqlize)
+const seqlize = new Sequelize(process.env.DATABASE_URL);
+// const seqlize=new Sequelize("champion","root",process.env.dbPassword,{
+//     host:"localhost",
+//     dialect:"mysql"
+// })
+
  
 module.exports = {seqlize}
