fb1494b5983f839b9b65dbda4027a9d61062794c
Payal Singh <112754449+itspayalsingh@users.noreply.github.com>
2024-06-18 12:16:59 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -6,7 +6,7 @@ const seqlize = new Sequelize(process.env.DATABASE_URL);
 // const seqlize=new Sequelize("champion","root",process.env.dbPassword,{
 //     host:"localhost",
 //     dialect:"mysql"
-// })
+// }
 
  
 module.exports = {seqlize}
